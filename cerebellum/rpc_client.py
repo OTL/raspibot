8ed2f5859d56b8a322fa5d851a49dca9c39d7224
Takashi Ogura <t.ogura@gmail.com>
2015-09-24 14:19:00 +0000

    Improve rpc timeout

--- cerebellum/rpc_client.py
+++ cerebellum/rpc_client.py
@@ -4,26 +4,41 @@
 from __future__ import print_function
 
 import socket
+import httplib
 import xmlrpclib
 
+class HTTP_with_timeout(httplib.HTTP):
+    def __init__(self, host='', port=None, strict=None, timeout=5.0):
+        if port == 0: port = None
+        self._setup(self._connection_class(host, port, strict, timeout=timeout))
+
+    def getresponse(self, *args, **kw):
+        return self._conn.getresponse(*args, **kw)
+
+class TimeoutTransport(xmlrpclib.Transport):
+    timeout = 0.5
+    def set_timeout(self, timeout):
+        self.timeout = timeout
+    def make_connection(self, host):
+        h = HTTP_with_timeout(host, timeout=self.timeout)
+        return h
 
 class CerebrumRpcClient(object):
     
     def __init__(self, uri='http://smilerobotics.com:12346'):
-        self._rpc = xmlrpclib.ServerProxy(uri, allow_none=True)
-        self._is_server_available = True
-
+        self._rpc = xmlrpclib.ServerProxy(uri, allow_none=True, transport=TimeoutTransport())
+ 
     def get_command(self, sensor_data):
-        if not self._is_server_available:
-            return {}
         try:
             socket.setdefaulttimeout(0.2)
+            print('rpc')
+            print(sensor_data)
             command = self._rpc.get_command(sensor_data)
+            print('rpc end')
             socket.setdefaulttimeout(None)
             return command
         except socket.error as e:
-            self._is_server_available = False
-            return {}
+             return {}
 
 
 if __name__ == '__main__':

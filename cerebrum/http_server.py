b37d0ca0ee108a2f79fa86a75b06c964f7f3a325
Takashi Ogura <t.ogura@gmail.com>
2016-03-08 04:03:55 +0900

    Make http_server jps node

--- cerebrum/http_server.py
+++ cerebrum/http_server.py
@@ -1,10 +1,16 @@
 # based on https://gist.github.com/n3wtron/4624820
 
+import threading
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from SocketServer import ThreadingMixIn
 import threading
 import StringIO
 import os.path
+import cv2
+import numpy
+import jps
+
 
 class HttpMjpegHandler(BaseHTTPRequestHandler):
     jpg_image = None
@@ -50,6 +56,7 @@ class HttpMjpegHandler(BaseHTTPRequestHandler):
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
 
+
 class HttpMjpegServer(object):
 
     def __init__(self):
@@ -63,3 +70,49 @@ class HttpMjpegServer(object):
     def close(self):
         print 'close http_server'
         self._server.socket.close()
+
+
+class DataWithLock():
+    def __init__(self, data=None):
+        self._lock = threading.Lock()
+        self._data = data
+
+    def set_data(self, data):
+        with self._lock:
+            self._data = data
+
+    def get_data(self):
+        with self._lock:
+            return self._data
+
+
+class JpsImageReceiver:
+    def __init__(self):
+        self._sub = jps.Subscriber('image', self.callback)
+        self.jpeg_image = DataWithLock()
+
+    def spin(self):
+        self._sub.spin()
+
+    def callback(self, msg):
+        jpg_data = numpy.fromstring(msg, dtype = "uint8")
+        self.jpeg_image.set_data(jpg_data)
+
+
+def main():
+    receiver = JpsImageReceiver()
+    mjpeg_server = HttpMjpegServer()
+    mjpeg_server.start()
+
+    dummy_img = cv2.imread('dummy.jpg')
+    receiver.jpeg_image.set_data(cv2.imencode(".jpeg", dummy_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100]))
+    HttpMjpegHandler.jpg_image = receiver.jpeg_image
+    try:
+        receiver.spin()
+    except KeyboardInterrupt:
+        pass
+    mjpeg_server.close()
+
+
+if __name__ == "__main__":
+    main()
